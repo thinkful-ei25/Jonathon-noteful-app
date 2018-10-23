0b595ff572b18ef06412aa9043894be773e73d61
Reldalk <Reldalk@gmail.com>
2018-10-23 15:01:11 -0400

    added basic queires

--- models/notes.js
+++ models/notes.js
@@ -2,10 +2,11 @@
 const mongoose = require('mongoose');
 
 const noteSchema = new mongoose.Schema({
-  title: {type: String, required: true},
+  title: { type: String, required: true },
   content: String
 });
 
+// Add `createdAt` and `updatedAt` fields
 noteSchema.set('timestamps', true);
 
 module.exports = mongoose.model('Note', noteSchema);
---
+++ scratch/queries.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const { MONGODB_URI } = require('../config');
+
+const Note = require('../models/notes');
+
+/*
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchTerm = 'cat';
+    let filter = {};
+
+    if (searchTerm) {
+      filter.title = { $regex: searchTerm };
+    }
+
+    return Note.find(filter).sort({ updatedAt: 'desc' });
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect()
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
+
+  */
+
+/*
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const searchId = '000000000000000000000006';
+    return Note.findById(searchId);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
+
+*/
+
+/*
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const newObject = {title: 'asdasdasdsad', content: 'sadasdasds'};
+    return Note.create(newObject);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
+  */
+/*
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const id = '000000000000000000000007';
+    const modifiedObject = Note.findById(id);
+    const newObject = {title: 'aaaaaaa'};
+
+    return Note.updateOne(newObject);
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
+  */
+ /*
+mongoose.connect(MONGODB_URI, { useNewUrlParser:true })
+  .then(() => {
+    const id = '000000000000000000000007';
+    const deletedObject = Note.findById(id);
+    return Note.deleteOne(deletedObject);
+    
+  })
+  .then(results => {
+    console.log(results);
+  })
+  .then(() => {
+    return mongoose.disconnect();
+  })
+  .catch(err => {
+    console.error(`ERROR: ${err.message}`);
+    console.error(err);
+  });
+  */
